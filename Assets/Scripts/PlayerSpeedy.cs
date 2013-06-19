4c960f6a187685261a835cbebefd22b28b420adc
jflory7 <jflory7@gmail.com>
2013-06-19 15:19:17 -0400

    Added boss script and edited menu

--- Assets/Scripts/PlayerSpeedy.cs
+++ Assets/Scripts/PlayerSpeedy.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlayerSpeedy : MonoBehaviour {
+public class PlayerSpeedy : MonoBehaviour 
+{
 
 	// Use this for initialization
 	void Start () 
@@ -12,11 +13,20 @@ public class PlayerSpeedy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		PlayerMovement ();
 		CheckBoundary ();
 	}
 	// Check the position of the ship compared to the boundaries.
-	public void CheckBoundary ()
+	public void PlayerMovement ()
 	{
+		if (Input.GetButton ("Up"))
+		{
+			transform.Translate(Vector3.up * 15 * Time.deltaTime, Space.World);
+		}
+		if (Input.GetButton ("Down"))
+		{
+			transform.Translate (Vector3.down * 15 * Time.deltaTime, Space.World);	
+		}
 		if (Input.GetButton ("Right"))
 		{
 			transform.Translate (15 * Vector3.left * Time.deltaTime);
@@ -26,7 +36,9 @@ public class PlayerSpeedy : MonoBehaviour {
 		{
 			transform.Translate (15 * Vector3.right * Time.deltaTime);	
 		}
-		
+	}
+	public void CheckBoundary ()
+	{
 		if (transform.position.x > 18)
 		{
 			transform.position = new Vector3 (18, transform.position.y, transform.position.z);	
@@ -37,5 +49,15 @@ public class PlayerSpeedy : MonoBehaviour {
 			transform.position = new Vector3 (-18, transform.position.y, transform.position.z);	
 		}
 		transform.position = new Vector3 (transform.position.x, transform.position.y, 24);
+		
+		if (transform.position.y  > -8)
+		{
+			transform.position = new Vector3 (transform.position.x, -8, transform.position.z);	
+		}
+		
+		if (transform.position.y < -13)
+		{
+			transform.position = new Vector3 (transform.position.x, -13, transform.position.z);	
+		}
 	}
 }
