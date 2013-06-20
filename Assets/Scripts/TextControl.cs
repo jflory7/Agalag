da2cb822874347f124281e2dc2f2c7a3e941785a
jflory7 <jflory7@gmail.com>
2013-06-20 14:44:41 -0400

    Added mouse detection to main menu.
    Also added explosion foundations for later

---
+++ Assets/Scripts/TextControl.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class TextControl : MonoBehaviour
+{
+	bool isQuitButton = false;
+	
+	// Detect the player's mouse.
+	public void OnMouseEnter ()
+	{
+		// Change the color of the text.
+		renderer.material.color = Color.red;
+	}
+	
+	// Detect the player's mouse.
+	public void OnMouseExit ()
+	{
+		// Change the color of the text.
+		renderer.material.color = Color.white;
+	}
+}
