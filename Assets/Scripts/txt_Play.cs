b0cb028427e87ff170ccd8e8465011b7464e64c0
jflory7 <jflory7@gmail.com>
2013-06-25 16:00:12 -0400

    Added level theme music to resources

--- Assets/Scripts/txt_Play.cs
+++ Assets/Scripts/txt_Play.cs
@@ -8,3 +8,26 @@ public class txt_Play : MonoBehaviour
 		Application.LoadLevel ("Level1");
 	}
 }
+
+//enum Fade {In, Out}
+//
+//var fadeTime = 4.0;
+//
+//function Start ()
+//{
+//    FadeAudio(fadeTime, Fade.Out);
+//}
+//
+//function FadeAudio (timer : float, fadeType : Fade) {
+//    var start = fadeType == Fade.In? 0.0 : 1.0;
+//    var end = fadeType == Fade.In? 1.0 : 0.0;
+//    var i = 0.0;
+//    var step = 1.0/timer;
+//
+//    while (i <= 1.0)
+//	{
+//        i += step * Time.deltaTime;
+//        audio.volume = Mathf.Lerp(start, end, i);
+//        yield;
+//    }
+//}
